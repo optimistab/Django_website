a018bd230c86fc656b440ae9b98df9b1ee4613a4
abhay <abhay.coder@gmail.com>
2020-04-23 12:36:23 +0530

    first version

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ABHAY AGRAWAL\Dev\trydjango\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/pages/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(*args,**kwargs):
+	return HttpResponse("<h1>Hello World</h1>")
+
+def contact_view(*args,**kwargs):
+	return HttpResponse("<h1>Contact Page</h1>")
+	
